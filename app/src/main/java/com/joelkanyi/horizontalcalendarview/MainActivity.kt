721543ab1033ed3da87deca29e597e527c177cd9
Ericgacoki <gacokieric@gmail.com>
2023-03-20 11:31:15 +0300

    - comment a duplicate calendar view

--- app/src/main/java/com/joelkanyi/horizontalcalendarview/MainActivity.kt
+++ app/src/main/java/com/joelkanyi/horizontalcalendarview/MainActivity.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.joelkanyi.horizontalcalendar.HorizontalCalendarView
 import com.joelkanyi.horizontalcalendarview.ui.theme.HorizontalCalendarViewTheme
 
@@ -24,32 +21,33 @@ class MainActivity : ComponentActivity() {
             val context = LocalContext.current
 
             HorizontalCalendarViewTheme {
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HorizontalCalendarView(
-                        modifier = Modifier.padding(16.dp),
-                        selectedTextColor = Color.White,
-                        unSelectedTextColor = Color.Black,
-                        selectedCardColor = Color.Blue,
-                        unSelectedCardColor = Color.LightGray,
-                        onDayClick = { day ->
-                            Toast.makeText(context, day.toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    )
+                    Column() {
+                        /*HorizontalCalendarView(
+                            modifier = Modifier.padding(16.dp),
+                            selectedTextColor = Color.White,
+                            unSelectedTextColor = Color.Black,
+                            selectedCardColor = Color.Blue,
+                            unSelectedCardColor = Color.LightGray,
+                            onDayClick = { day ->
+                                Toast.makeText(context, day.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                        )*/
 
-                    HorizontalCalendarView(
-                        modifier = Modifier,
-                        selectedTextColor = Color.White,
-                        unSelectedTextColor = Color.Black,
-                        selectedCardColor = Color.Blue,
-                        unSelectedCardColor = Color.LightGray,
-                        onDayClick = { day ->
-                            Toast.makeText(context, day.toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    )
+                        HorizontalCalendarView(
+                            modifier = Modifier,
+                            selectedTextColor = Color.White,
+                            unSelectedTextColor = Color.Black,
+                            selectedCardColor = Color.Blue,
+                            unSelectedCardColor = Color.LightGray,
+                            onDayClick = { day ->
+                                Toast.makeText(context, day.fullDate, Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                    }
                 }
             }
         }
